30577279fc2037cf97f30256e51f277e04bc6144
Micha Ela M. Masocol <156573630+mchlamscl27@users.noreply.github.com>
2024-07-01 00:08:43 +0800

    Delete MY PROJECTS/LoginForm/LoginForm directory

--- MY PROJECTS/LoginForm/LoginForm/MODULE/BasicFunction.vb
+++
@@ -1,6 +0,0 @@
-﻿Module BasicFunction
-    Public Function GenerateRandomNumber() As Integer
-        Dim random As New Random()
-        Return random.Next(100000, 999999)
-    End Function
-End Module
